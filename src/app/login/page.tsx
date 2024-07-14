342a6cae5686728968c50ba79e43a754324ff168
piyawong <piyawongmahat@hotmail.com>
2024-07-14 21:51:11 +0700

    test package

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { useAuth } from '../../module/auth/AuthProvider';
-import styles from './page.module.css';
-import LoginForm from '@/components/LoginForm';
-
+import { useAuth } from "../../module/auth/AuthProvider";
+import styles from "./page.module.css";
+import LoginForm from "@/components/LoginForm";
+import packageJsonWeb from "../../../web/package.json";
 export default function Home() {
   const { signin } = useAuth();
-
+  console.log("packageJsonWeb = ", packageJsonWeb);
   return (
     <main className={styles.main}>
       <LoginForm signin={signin} />
